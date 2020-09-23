13a24bc7f816c0413c268b38ac1f2b5d1ed17af6
aaron <aarn.gmpc@gmail.com>
2020-09-23 22:03:21 +0800

    added c++ files for sync

--- c++/bryan.cpp
+++ c++/bryan.cpp
@@ -1,18 +1,5 @@
-	#include<iostream>
-	using namespace std;
-    static int factorial(int n){
-        if (n == 1){
-            return 1;
-        }
-        else{
-            return n * factorial(n-1);
-        }
-    }
-	int main()
-	{
-        int factorialLimit;
-        cout << "How big is your factorial";
-        cin >> factorialLimit;
-        cout << factorial(factorialLimit);
-        return 0;
-	}
+#include<iostream>
+using namespace std;
+int main(){
+    return 1;
+}
