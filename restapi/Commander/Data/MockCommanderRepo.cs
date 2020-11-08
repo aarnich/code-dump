1c3088e88f8ad03b910d2750531a5ce2156b02e9
aaron <aarn.gmpc@gmail.com>
2020-11-08 22:47:36 +0800

    RestAPI Testing

---
+++ restapi/Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+namespace Commander.Data
+{
+
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 1, HowTo = "test1", Line = "testline1", Platform = "testplatform1" },
+                new Command { Id = 2, HowTo = "test2", Line = "testline2", Platform = "testplatform2" },
+                new Command { Id = 3, HowTo = "test3", Line = "testline3", Platform = "testplatform3" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "test", Line = "testline", Platform = "testplatform" };
+        }
+    }
+}
